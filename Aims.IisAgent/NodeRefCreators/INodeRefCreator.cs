2528e4279011eda19f742e112553151b26c49ae4
Urii <polkilok2@gmail.com>
2017-05-23 13:22:06 +0300

    create decorator-based class hierarchy, rewrite code to use it

---
+++ Aims.IisAgent/NodeRefCreators/INodeRefCreator.cs
@@ -0,0 +1,10 @@
+﻿using Aims.Sdk;
+
+namespace Aims.IisAgent.NodeRefCreators
+{
+	public interface INodeRefCreator<in TBaseObject>
+	{
+		NodeRef CreateNodeRefFromObj(TBaseObject obj);
+		NodeRef CreateFromInstanceName(string instanceName);
+	}
+}
